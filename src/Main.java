358e38daad62f4be9b3a87c6421edaa78ad208fd
Márta Krisztián <martakrisztian97@gmail.com>
2022-03-11 13:52:56 +0100

    Update comments

--- src/Main.java
+++ src/Main.java
@@ -35,13 +35,8 @@ public class Main {
         System.out.println("peek(): "+stack.peek());
         System.out.println(stack);
 
-        /* Returns the 1-based position where an object is on this stack. If the object o occurs as an item in this stack,
-         this method returns the distance from the top of the stack of the occurrence nearest the top of the stack;
-         the topmost item on the stack is considered to be at distance 1.
-         The equals method is used to compare o to the items in this stack.
-         Az 1 alapú pozíciót adja vissza, ahol egy objektum ebben a veremben van. Ha az o objektum elemként szerepel ebben a veremben,
-         akkor ez a metódus a verem tetejétől a verem tetejéhez legközelebb eső előfordulás távolságát adja vissza;
-         a verem legfelső eleme az 1-es távolságban található. Az egyenlő módszerrel o-t hasonlítanak össze a veremben lévő elemekkel.
+        /* Returns the 1-based position where an object is on this stack.
+         Visszaadja az elem helyzetét a verem tetejéről.
         */
         System.out.println("\n"+stack);
         System.out.println("search(W): "+stack.search('W'));
